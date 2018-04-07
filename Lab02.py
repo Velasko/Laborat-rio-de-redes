496daf3f37959121d9855bdeaa9cd19a33339db8
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2018-04-07 13:53:14 -0300

    comentários e melhor separação entre as questões

--- Lab02.py
+++ Lab02.py
@@ -23,16 +23,18 @@ def questao1():
              "github.com",              #10
              )
 
-    requests_returns = {}
+    requests_returns = {} #dicionario para receber o código e metainformações de cada página
+    #preenchimento de requests_returns e print das metainformações
     for site in sites:
         try:
             req = request.urlopen("http://{}".format(site))
             requests_returns[site] = [str(req.info()), req.getcode()]
-            print('{2}{0}{3}\n\n{1}'.format(site, meta, bolt_in, bolt_out))
+            print('{2}{0}{3}\n\n{1}'.format(site, requests_returns[site][0], bolt_in, bolt_out))
         except url.error.HTTPError as e:
             requests_returns[site] = [e, req.getcode()]
             print('{2}{0}:{3} {1}\n\n'.format(site, e, bolt_in, bolt_out))
 
+    #print do código de cada página
     for site, (info, code) in requests_returns.items():
         print("Site: {:<30} código: {}".format(site, code))
 
@@ -40,6 +42,7 @@ def questao1():
 
 
 def process_data(data):
+    '''método para retornar o logradouro de data'''
     datas = data.decode('utf-8').split('\n')[1:-1]
     for data in datas:
         key, item = data.split(':')
@@ -47,6 +50,9 @@ def process_data(data):
             return item.split('"')[1]
 
 def questao2a():
+
+    print("{}Execução da questão 2(a){}\n".format(bolt_in, bolt_out))
+
     main_link = "http://viacep.com.br/ws/{}/json"
     ceps = ('60130301',
             '60130304',
@@ -54,31 +60,42 @@ def questao2a():
             '60125001'
             )
 
+    #cada cep em ceps é acessado e é printado seu logadouro
     for cep in ceps:
         data = request.urlopen(main_link.format(cep)).read()
         log = process_data(data)
         print("CEP: {:<10} Logradouro: {}".format(cep[:5]+"-"+cep[5:],
                                                   log))
 
+    print("\n{}Fim da execução da segunda questão(a){}\n".format(bolt_in, bolt_out))
+
 def questao2b():
+
+    print("{}Execução da questão 2(b){}\n".format(bolt_in, bolt_out))
+
     research_main = 'https://www.google.com.br/search?q={}'
     termo_pesquisa = ('batata',)
 
+    #criação de um opener que diz ser um browser firefox
     opener = request.FancyURLopener()
     opener.version = "Mozilla/5.0"
 
     for termo in termo_pesquisa:
         try:
-            x = request.urlopen(research_main.format(termo))
+            pesquisa = research_main.format(termo)
+            x = request.urlopen(pesquisa)
             return(x.read())
         except url.error.HTTPError as e:
-            print(e)
+            print("Pesquisa por \"{0}\", gera o link\n{1}\nretornou {2}\n".format(termo,
+                                                                             pesquisa,
+                                                                             e))
         except url.error.URLError as e:
             print(e)
 
         research = opener.open(research_main.format(termo))
 
-        return research
+    print("{}Fim da execução da segunda questão(b){}\n".format(bolt_in, bolt_out))
+
 
 
 if __name__ == "__main__":
@@ -91,4 +108,6 @@ if __name__ == "__main__":
         bolt_in = ""
         bolt_out = ""
 
-    x=questao2b()
+    questao1()
+    questao2a()
+    questao2b()
