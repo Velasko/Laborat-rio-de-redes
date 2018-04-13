22d726c6936c705065e5a860ccf5e9f07eb7aaae
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2018-04-12 21:29:53 -0300

    Correção
    Adicionado comentário explicando metainformações.
    Removido a função process_data, biblioteca json agora utilizada.
    Adicionado soloção do item 2.b).

--- Lab02.py
+++ Lab02.py
@@ -7,6 +7,7 @@ Dupla:
 import sys
 import urllib as url
 from urllib import request
+import requests as req
 
 def questao1():
     print("{}Execução da questão 1{}\n".format(bolt_in, bolt_out))
@@ -24,6 +25,7 @@ def questao1():
              )
 
     requests_returns = {} #dicionario para receber o código e metainformações de cada página
+
     #preenchimento de requests_returns e print das metainformações
     for site in sites:
         try:
@@ -34,20 +36,16 @@ def questao1():
             requests_returns[site] = [e, req.getcode()]
             print('{2}{0}:{3} {1}\n\n'.format(site, e, bolt_in, bolt_out))
 
+##        Uma das possíveis Metainformações é 'Server', no qual diz qual é o servidor
+##    utilizado e qual a sua versão. Exemplo:
+##    'Server: Apache/2.4.25 (Amazon) PHP/5.5.38'.
+
     #print do código de cada página
     for site, (info, code) in requests_returns.items():
         print("Site: {:<30} código: {}".format(site, code))
 
     print("{}Fim da execução da primeira questão{}\n".format(bolt_in, bolt_out))
 
-
-def process_data(data):
-    '''método para retornar o logradouro de data'''
-    datas = data.decode('utf-8').split('\n')[1:-1]
-    for data in datas:
-        key, item = data.split(':')
-        if key.split('"')[1] == "logradouro":
-            return item.split('"')[1]
 
 def questao2a():
 
@@ -62,10 +60,10 @@ def questao2a():
 
     #cada cep em ceps é acessado e é printado seu logadouro
     for cep in ceps:
-        data = request.urlopen(main_link.format(cep)).read()
-        log = process_data(data)
-        print("CEP: {:<10} Logradouro: {}".format(cep[:5]+"-"+cep[5:],
-                                                  log))
+        base_zip_code=(main_link.format(cep))
+        req_base = req.get(base_zip_code)
+        dados = req_base.json()
+        print("CEP: {:<10} Logradouro: {}".format(cep[:5]+"-"+cep[5:],dados['logradouro']))
 
     print("\n{}Fim da execução da segunda questão(a){}\n".format(bolt_in, bolt_out))
 
@@ -76,9 +74,7 @@ def questao2b():
     research_main = 'https://www.google.com.br/search?q={}'
     termo_pesquisa = ('batata',)
 
-    #criação de um opener que diz ser um browser firefox
-    opener = request.FancyURLopener()
-    opener.version = "Mozilla/5.0"
+    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"}
 
     for termo in termo_pesquisa:
         try:
@@ -92,11 +88,18 @@ def questao2b():
         except url.error.URLError as e:
             print(e)
 
-        research = opener.open(research_main.format(termo))
+##    Não, retornou erro 403 (não foi permitido o acesso),Error 403 é um código de erro HTTP 
+##    retornado pelo servidor web quando o utilizador ou programa tenta obter acesso a um recurso 
+##    que o servidor não permite. 
+##    Solução: Adicionar um header, de forma que o google identifique o programa executado como
+##    um usuário
+
+        req = request.Request(pesquisa, headers=headers)
+        resp = request.urlopen(req)
+        repData = resp.code
+        print("Com a adição de um header, temos o código: {}\n".format(repData))
 
     print("{}Fim da execução da segunda questão(b){}\n".format(bolt_in, bolt_out))
-
-
 
 if __name__ == "__main__":
 
